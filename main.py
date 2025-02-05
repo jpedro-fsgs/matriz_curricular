97b8957924ac049349018db17aa1cb8c30c82479
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-02-04 23:11:18 -0300

    feat: disciplinas disponíveis

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ class Disciplina:
         self.nome = nome
         self.completada = False;
         self.pre_requisitos = []
+        self.requisito_para = []
 
     def add_pre_requisito(self, disciplina):
         self.pre_requisitos.append(disciplina)
 
+    def add_requisito_para(self, disciplina):
+        disciplina.requisito_para.append(self)
+
+    def is_disponivel(self):
+        return not self.completada and (
+            not len(self.pre_requisitos) or
+            all([pre_requisito.completada for pre_requisito in self.pre_requisitos])
+            )
+    
+    def set_completado(self):
+        self.completada = True
+
     def __str__(self):
-        string = f"{self.id}: {self.nome} - {'✓' if self.completada else 'X'}\n"\
-                 f"{'Pré-requisitos\n' if self.pre_requisitos else ''}"\
-                 f"{'\n'.join(["\t" + str(pre_requisito) for pre_requisito in self.pre_requisitos])}"
+        string = f"{self.id}: {self.nome} - {'✓' if self.completada else 'X'}\n"
         return string
-                
 
 
 class Matriz:
@@ -29,8 +39,9 @@ class Matriz:
             disciplina = disciplinas[disciplina_json["id"]]
             for pre_requisito in disciplina_json["pre_requisitos"]:
                 disciplina.add_pre_requisito(disciplinas[pre_requisito])
+                disciplinas[pre_requisito].add_requisito_para(disciplina)
 
-        # print(disciplinas)
+
         self.disciplinas = disciplinas
 
     def __str__(self):
@@ -38,8 +49,18 @@ class Matriz:
             str(disciplina)
             for disciplina in self.disciplinas.values()
         ])
+    
+    def set_completado(self, id):
+        self.disciplinas[id].set_completado()
 
+    
+    def completadas(self):
+        return [disciplina for disciplina in self.disciplinas.values() if disciplina.completada]
 
+    def disponiveis(self):
+        lista_disponiveis = [disciplina for disciplina in self.disciplinas.values() if disciplina.is_disponivel()]
+        print(f"{len(lista_disponiveis)} disciplinas disponíveis.")
+        return lista_disponiveis
 
 
 with open('matriz.json', 'r') as file:
@@ -47,4 +68,8 @@ with open('matriz.json', 'r') as file:
 
 matriz = Matriz(data)
 
-print(matriz)
+for id in [3, 4, 5, 7, 14, 18, 20, 10, 15, 22, 6]:
+    matriz.set_completado(id)
+
+for i in matriz.disponiveis():
+    print(i)
